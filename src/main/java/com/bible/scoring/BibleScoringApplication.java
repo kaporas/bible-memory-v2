2189f4438566fa29994624fbd6eccbad1951da93
kaporas <kaporas@naver.com>
2026-04-23 19:37:39 +0900

    Add WAR packaging and update dependencies in pom.xml; extend BibleScoringApplication for servlet support; increase image upload size limit; refine text in HTML templates

--- src/main/java/com/bible/scoring/BibleScoringApplication.java
+++ src/main/java/com/bible/scoring/BibleScoringApplication.java
@@ -2,9 +2,16 @@ package com.bible.scoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BibleScoringApplication {
+public class BibleScoringApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BibleScoringApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BibleScoringApplication.class, args);
